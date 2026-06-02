1381048a4aff599b639cde25a69cc1153337deb3
vivek <vivekkumarvk7437399@gmail.com>
2026-06-03 00:20:50 +0530

    sdfwes

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const cert = (name: string) =>
 export default defineConfig({
   plugins: [react()],
 
-  // base: '/videoConferencing/',
+  base: '/videoConferencing/',
 
   server: {
     // Listen on all interfaces so other devices on the LAN can reach it.
